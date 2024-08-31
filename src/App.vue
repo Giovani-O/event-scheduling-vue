277f8f94365ec396bfaf8379880582ed0f972b82
Giovani <giovani98@proton.me>
2024-08-31 15:35:05 -0300

    feat: Create EventCard component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <EventCard
+        v-for="(i, index) in 9"
+        :key="index"
+        title="Lorem Ipsum 2024"
+        when="2024-10-01"
+        description="Lorem Ipsum Dolor Sit Amet"
+        @register="console.log('Tenoí')"
+      />
+    </section>
     <h2 class="text-2xl font-medium">Your Events</h2>
   </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="border border-zinc-800 bg-zinc-900 rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-zinc-800">{{ title }}</h3>
+    <p class="p-4 border-b border-zinc-800">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+
+    <section class="flex justify-end p-4">
+      <button
+        class="text-sm font-medium border border-zinc-800 rounded-full px-3 py-1 bg-zinc-950 hover:bg-zinc-900"
+        @click="$emit('register')"
+      >
+        Register
+      </button>
+    </section>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String
+})
+
+defineEmits(['register'])
+</script>
